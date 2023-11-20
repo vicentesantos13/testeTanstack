9d377a544006216b2d6b7e7eec22c18f4e4d7962
Vicente Santos <vicentemoises.santos@gmail.com>
2023-11-20 10:45:33 -0300

    first

---
+++ src/utils/providers.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { QueryClientProvider } from '@tanstack/react-query';
+import {ReactNode} from 'react';
+import { queryClient } from './queryClient';
+type Props = {
+    children: ReactNode;
+}
+
+export const Providers = ({children}:Props)=>{
+    return(
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
